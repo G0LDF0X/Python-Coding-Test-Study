72d58b5cef050f16459c01f1e71a85d092cde891
SinMoonGh <109773691+SinMoonGh@users.noreply.github.com>
2024-07-09 09:10:13 +0900

    구명보트  오류 미해결, 괄호.....너무 어려워...#

--- myeonghoon/괄호.py
+++ myeonghoon/괄호.py
@@ -5,4 +5,15 @@
 # b 포인트가 f+1 이면 참
 # f 포인트가 가리키는 문자가 
 
-left_parentheses = 
+left_parentheses = ['(', '{', '['] # 참이 되는 조합은 11, 22, 33
+right_parentheses = [')', '}', ']']
+
+# x의 값만큼 배열을 왼쪽으로 회전시키는 함수
+def left_lotation(arr):
+    # 맨 앞에 있는 0인덱스를 끝으로 위치를 옮겨주면 된다.
+    start = 0
+    # end = start-1 # null을 먼저 이동시켜야 되나?
+
+    move_par = arr.pop(start)
+    arr.append(move_par)
+    return arr
