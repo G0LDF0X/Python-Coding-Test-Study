79d44b6cf3c2e0416f82fc6b9fe30662aed4a8b5
choi-jyoon <72444141+choi-jyoon@users.noreply.github.com>
2024-07-24 08:31:49 +0900

    0724 문제풀이

--- jeongyun/2805나무자르기.py
+++ jeongyun/2805나무자르기.py
@@ -1,33 +1,21 @@
 import sys
 
-def binary_search(tree, start, high):
-    global result
-    mid = (start + high) // 2
-    cut_tree = 0
-    
-    if start > high:
-        return result
-    
-    for t in tree:
-        if t - mid < 0:
-            break
-        else:
-            cut_tree += t - mid
-    
-    if cut_tree >= m:
-        result = max(result, mid)
-    
-    if cut_tree > m:
-        return binary_search(tree, mid + 1, high)
-    else:
-        return binary_search(tree, start, mid - 1)
-
-
 n, m = map(int, sys.stdin.readline().split())
-tree = [int(sys.stdin.readline()) for _ in range(n)]
+tree = list(map(int, sys.stdin.readline().split()))
+s = 1
+e = max(tree)
 
-tree.sort(reverse=True)
-MAX = tree[0]
-
-print(binary_search(tree, 0, MAX))
+while s <= e:
+    mid = (s + e) // 2
+    
+    cut = 0 
+    for t in tree:
+        if t >= mid:
+            cut += (t - mid)
+    if cut >= m:
+        s = mid+1
+    else:
+        e = mid-1
+        
+print(e)
 
