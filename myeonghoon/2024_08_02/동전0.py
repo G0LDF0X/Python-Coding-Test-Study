bcbcb1700d857e0476f2d8c059b157d5a0069689
SinMoonGh <109773691+SinMoonGh@users.noreply.github.com>
2024-08-02 05:27:31 +0900

    한줄로 서기 까지

---
+++ myeonghoon/2024_08_02/동전0.py
@@ -0,0 +1,39 @@
+# 가지고 있는 동전의 갯수 : N개
+# 지불해야 되는 금액 : K원
+# 필요한 동전의 최소 갯수를 구하라.
+
+# 4200원 -> 50000원을 4200원보다 큼, 10000원도 4200원보다 큼, 5000원도 4200원보다 큼, 1000원은 4200원보다 작음
+# 4200원을 1000원으로 나눈 나머지는 잔돈 200원
+# 1000원으로 나눈 나머지 값이 200원이기때문에 1000원보다 낮은 동전을 지불해야 한다. 1000원보다 낮은 동전은 500원이다.
+# 500원은 200원보다 큼, 100원은 200보다 작음. 200 / 100하면 나머지가 0이고, 몫이 2가됨. 이건 동전의 갯수임
+# 4200 / 1000 = 4
+# 200 / 100 = 2
+# 답은 6이다.
+
+
+import sys
+
+sys.setrecursionlimit(10**6)
+input = sys.stdin.readline
+
+N, K = map(int, input().split())
+
+
+def solution(N, K):
+    answer = 0
+    coin_list = []
+
+    for _ in range(N):
+        coin_list.append(list(map(int, input().split())))
+
+    coin_list = sorted(coin_list, reverse=True)
+
+    for i in range(N):
+        if coin_list[i][0] < K:
+            answer += int(K / coin_list[i][0])
+            K = K % coin_list[i][0]
+
+            if K == 0:
+                return answer     
+
+print(solution(N, K))
---
+++ myeonghoon/2024_08_02/컵홀더.py
@@ -0,0 +1,34 @@
+# 컵 홀더 문제는 S : 개인석, L : 커플석이 있다.
+# 개인석만 존재할 경우 *S*S*S* , 컵홀더의 갯수 = 개인석의 갯수 + 1
+# 개인석과 커플석이 같이 있을 경우 *S*LL*S*, 컵홀더의 갯수 = (개인석의 갯수 + 1) - 커플석의 갯수
+# 
+
+import sys
+
+sys.setrecursionlimit(10**6)
+input = sys.stdin.readline
+
+N = int(input().strip())
+sit = input().strip()
+
+
+
+def solution(N, sit):
+    answer = 0
+    count_ll = 0
+    cup_count=0
+
+    split_s = sit.split('S')
+
+    for ll in split_s:    
+        count_ll += ll.count('LL')
+
+    cup_count = len(sit) +1 - count_ll
+
+    if N <= cup_count:
+        return N
+    elif N > cup_count:
+        return cup_count 
+
+
+print(solution(N, sit))
